4a96237c319afdfb4887c1ba4abc5d6d16760ae2
BubullTech <alozygaetan@gmail.com>
2021-04-16 13:55:21 +0200

    button1

--- app/src/main/java/fr/fscript98/zapette/ButtonRepository.kt
+++ app/src/main/java/fr/fscript98/zapette/ButtonRepository.kt
@@ -1,24 +1,28 @@
 package fr.fscript98.zapette
 
+import android.widget.Toast
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import fr.fscript98.zapette.ButtonRepository.Singleton.buttonsList
 import fr.fscript98.zapette.ButtonRepository.Singleton.databaseRef
+import fr.fscript98.zapette.ButtonRepository.Singleton.getcode
 
 class ButtonRepository {
 
     object Singleton {
         //se connecter à la reference "buttons"
-        val databaseRef = FirebaseDatabase.getInstance().getReference("buttons")
+        val databaseRef = FirebaseDatabase.getInstance().getReference("Questionnaire")
 
+        val getcode = arrayListOf<QuestionnaireModel>()
         //créer une liste qui va contenir les buttons
         val buttonsList = arrayListOf<VoteButtonModel>()
+
     }
 
     //update un objet button dans la bdd
-    fun updateButton(button:VoteButtonModel){
+    /*fun updateButton(button:VoteButtonModel){
         databaseRef.child(button.id).setValue(button.nbVotes++)
-    }
+    }*/
 }
---
+++ app/src/main/java/fr/fscript98/zapette/QuestionnaireModel.kt
@@ -0,0 +1,5 @@
+package fr.fscript98.zapette
+
+class QuestionnaireModel(
+    val motdepasse: Int = 12345)
+
