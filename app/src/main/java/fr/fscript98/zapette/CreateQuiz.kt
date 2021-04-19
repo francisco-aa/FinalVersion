cf47fafe5e9a84b44f0a9f2367df553f398325d3
aboubaker <aboubakermirghani@gmail.com>
2021-04-19 15:01:56 +0200

    J'ai fait la generation de code à 5 chiifres et le qr code

--- app/src/main/java/fr/fscript98/zapette/CreateQuiz.kt
+++ app/src/main/java/fr/fscript98/zapette/CreateQuiz.kt
@@ -3,7 +3,22 @@ package fr.fscript98.zapette
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import kotlin.random.Random
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.qrcode.QRCodeWriter
+
+import com.journeyapps.barcodescanner.BarcodeEncoder
+
+
+
+
+
+
 
 class CreateQuiz : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +30,27 @@ class CreateQuiz : AppCompatActivity() {
             val intentButtonBack = Intent(this, TeacherBoard::class.java)
             startActivity(intentButtonBack)
         }
+
+        val myRandomInt = Random.nextInt(10000, 100000)
+        val textView = findViewById<TextView>(R.id.textView2)
+        val qrCode = QRCodeWriter()
+        //val qrCode = MultiFormatWriter()
+
+        val bitMtx = qrCode.encode(
+            "$myRandomInt",
+            BarcodeFormat.QR_CODE,
+            100,
+            100
+        )
+        val imageCode = findViewById<ImageView>(R.id.imageQrCode)
+        val buttonCode = findViewById<Button>(R.id.btnGenerate)
+        val barcodeEncoder = BarcodeEncoder()
+        val bitmap = barcodeEncoder.createBitmap(bitMtx)
+        buttonCode.setOnClickListener {
+            textView.text=("$myRandomInt")
+            //MatrixToImageWriter.(bitMtx, "PNG", imageCode)
+            imageCode.setImageBitmap(bitmap)
+        }
+
     }
 }
