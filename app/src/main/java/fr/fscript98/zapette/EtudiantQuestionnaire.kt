43f73b5a3e16d624a35d31eea1906c585d7a26a6
Almousleh <andrmous123@gmail.com>
2021-04-19 11:16:43 +0200

    test 1

--- app/src/main/java/fr/fscript98/zapette/EtudiantQuestionnaire.kt
+++ app/src/main/java/fr/fscript98/zapette/EtudiantQuestionnaire.kt
@@ -1,10 +1,11 @@
 package fr.fscript98.zapette
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class EtudiantQuestionnaire : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,5 +24,52 @@ class EtudiantQuestionnaire : AppCompatActivity() {
             val intent = Intent(this, EtudiantRepondre::class.java)
             startActivity(intent)
         }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkPermission(context , permissions)) {
+                Toast.makeText(context , "permission are already provided" , Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                requestPermissions(permissions , PERMISSION_REQUEST)
+
+            }
+        } else {
+            Toast.makeText(context , "permission are already provided" , Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun checkPermission(context: Context, permissionArray Array<string>): Boolean {
+
+        var allSuccess = true
+
+        for (i in permissionArray.indices){
+
+            if(checkCallingOrSelfPermission(permissionArray[i])-PackageManager PERMISSION_DENTED) {
+
+                allSuccess = false
+            }
+        }
+        return allSuccess
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == PERMISSION_REQUEST) {
+            var allSuccess = true
+            for(i in permissions.indices){
+                if(grantResults[i] == PackageManager.PERMISSION_DENIED{
+                        allSuccess = false
+                        var requestAgain = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && shouldShowRequestPermissionRationale(permissions[i])}
+                    if (requestAgain) {
+                        Toast.makeText(context , "permission denied" , Toast.LENGTH_SHORT)
+                            .show()
+                    }else{
+                        Toast.makeText(context,"go to settings and enable the permission",Toast.LENGTH_SHORT).show()
+                    }
+            }
+            if (allSuccess)
+                Toast.makeText(context,"permissions grante",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
     }
 }
