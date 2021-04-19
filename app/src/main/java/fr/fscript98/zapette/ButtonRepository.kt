49e29f1f79c7dcefd41f39b358763482f396d78e
BubullTech <alozygaetan@gmail.com>
2021-04-19 10:53:58 +0200

    Repondre

--- app/src/main/java/fr/fscript98/zapette/ButtonRepository.kt
+++ app/src/main/java/fr/fscript98/zapette/ButtonRepository.kt
@@ -1,25 +1,41 @@
 package fr.fscript98.zapette
 
-import android.widget.Toast
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import fr.fscript98.zapette.ButtonRepository.Singleton.buttonsList
-import fr.fscript98.zapette.ButtonRepository.Singleton.databaseRef
-import fr.fscript98.zapette.ButtonRepository.Singleton.getcode
+
 
 class ButtonRepository {
 
-    object Singleton {
-        //se connecter à la reference "buttons"
-        val databaseRef = FirebaseDatabase.getInstance().getReference("Questionnaire")
 
-        val getcode = arrayListOf<QuestionnaireModel>()
-        //créer une liste qui va contenir les buttons
-        val buttonsList = arrayListOf<VoteButtonModel>()
+    //se connecter à la reference "buttons"
+    var databaseRef = FirebaseDatabase.getInstance().reference
+
+    val getcode = arrayListOf<QuestionnaireModel>()
+    //créer une liste qui va contenir les buttons
+    val buttonsList = arrayListOf<VoteButtonModel>()
+
+
+
+    var getdata = object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot){
+            for (ds in snapshot.children){
+                val code= ds.getValue(VoteButtonModel::class.java)
+                if(code!=null){
+                    buttonsList.add(code)
+                }
+            }
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            TODO("Not yet implemented")
+        }
 
     }
+
+
+
 
     //update un objet button dans la bdd
     /*fun updateButton(button:VoteButtonModel){
--- app/src/main/java/fr/fscript98/zapette/VoteButtonModel.kt
+++ app/src/main/java/fr/fscript98/zapette/VoteButtonModel.kt
@@ -1,6 +1,6 @@
 package fr.fscript98.zapette
 
 class VoteButtonModel(
-    val id: String = "buttonX",
-    var nbVotes: Int = 0
+    val motdepasse: String = "11"
+
 )
