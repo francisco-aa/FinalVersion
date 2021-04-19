6af4984b31a52dd03bc17512793e40a635aa7fbe
BubullTech <alozygaetan@gmail.com>
2021-04-19 13:48:49 +0200

    Y a tout

--- app/src/main/java/fr/fscript98/zapette/EtudiantQuestionnaire.kt
+++ app/src/main/java/fr/fscript98/zapette/EtudiantQuestionnaire.kt
@@ -8,7 +8,6 @@ import android.widget.ImageView
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.FirebaseDatabase
 
 
 class EtudiantQuestionnaire() : AppCompatActivity() {
@@ -17,46 +16,54 @@ class EtudiantQuestionnaire() : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_etudiant_questionnaire)
 
-        val database = FirebaseDatabase.getInstance()
-        val ref_questionnaire = database.getReference("questionnaire")
+        //val database = FirebaseDatabase.getInstance()
+        //val ref_questionnaire = database.getReference("questionnaire")
         val intent = Intent(this, MainActivity::class.java)
         val intent2 = Intent(this, EtudiantRepondre::class.java)
         var codeUtilisateur = "12345"
-        var buttonsList = arrayListOf<VoteButtonModel>()
-        val editText = findViewById<EditText>(R.id.zone_saisie_code)
-        val codesaisi = editText.text.toString()
+
+
+
+
 
         //Récupérer le code saisi par l'utilisateur
 
+
         //mettre a jour la liste de plant
+
         val backbutton = findViewById<ImageView>(R.id.button_back)
         backbutton.setOnClickListener{
             startActivity(intent)
         }
         val buttonRejoindre = findViewById<Button>(R.id.button_rejoindre)
         buttonRejoindre.setOnClickListener {
+
+            val editText = findViewById<EditText>(R.id.zone_saisie_code)
             val codesaisi = editText.text.toString()
+
                 //TODO: Si codesaisi vide, redémarrer l'activity avec message d'erreur vide
-            ref_questionnaire.get().addOnSuccessListener {
-                for (ds in it.children){
-                    var codeBDD= ds.getValue(VoteButtonModel::class.java)
-                    if (codeBDD!=null){
-                        buttonsList.add(codeBDD)
-                        //Toast.makeText(applicationContext, codeBDD.motdepasse.toString(), LENGTH_SHORT).show()
-                    }
-                }
+                /*ref_questionnaire.child("question").get().addOnSuccessListener {
+                codeBDD = it.value.toString()
+                Toast.makeText(applicationContext, codeBDD, LENGTH_SHORT).show()
             }.addOnFailureListener{
                 Toast.makeText(applicationContext, "Ya pa de le code", LENGTH_SHORT).show()
-            }
-            for (tu in buttonsList) {
-                val codesaisi = editText.text.toString()
-                Toast.makeText(applicationContext, codesaisi, LENGTH_SHORT).show()
-                if (codesaisi == tu.motdepasse.toString()) {
+            }*/
 
-                    startActivity(intent2)
-                } else {
-                }
+
+
+
+            if (codesaisi == codeUtilisateur) {
+                Toast.makeText(applicationContext , "oui" , LENGTH_SHORT).show()
+                startActivity(intent2)
+            } else {
+                            Toast.makeText(applicationContext , "non" , LENGTH_SHORT).show()
             }
+
+
+
         }
+
+
     }
+
 }
