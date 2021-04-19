72e1c9b47f009bd32730305bd15adc918ea9dcf0
Almousleh <andrmous123@gmail.com>
2021-04-19 11:52:38 +0200

    scanner le QR code c'est bon

--- app/src/main/java/fr/fscript98/zapette/EtudiantQuestionnaire.kt
+++ app/src/main/java/fr/fscript98/zapette/EtudiantQuestionnaire.kt
@@ -1,10 +1,13 @@
 package fr.fscript98.zapette
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
 
 class EtudiantQuestionnaire : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,17 +15,38 @@ class EtudiantQuestionnaire : AppCompatActivity() {
         setContentView(R.layout.activity_etudiant_questionnaire)
 
         val buttonBack = findViewById<ImageView>(R.id.button_back4)
-        buttonBack.setOnClickListener{
-            val intentButtonBack = Intent(this, MainActivity::class.java)
+        buttonBack.setOnClickListener {
+            val intentButtonBack = Intent(this , MainActivity::class.java)
             startActivity(intentButtonBack)
 
         }
 
         val buttonRejoindre = findViewById<Button>(R.id.button_rejoindre)
-        buttonRejoindre.setOnClickListener{
-            val intent = Intent(this, EtudiantRepondre::class.java)
+        buttonRejoindre.setOnClickListener {
+            val intent = Intent(this , EtudiantRepondre::class.java)
             startActivity(intent)
         }
-
+        val button_scan = findViewById<Button>(R.id.button_scan)
+        button_scan.setOnClickListener() {
+            val scanner = IntentIntegrator(this)
+            scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES)
+            scanner.setBeepEnabled(false)
+            scanner.initiateScan()
+        }
+    }
+    override fun onActivityResult(requestCode: Int , resultCode: Int , data: Intent?) {
+        super.onActivityResult(requestCode , resultCode , data)
+        if (resultCode == Activity.RESULT_OK) {
+            val result = IntentIntegrator.parseActivityResult(requestCode , resultCode , data)
+            if (result != null) {
+                if (result.contents == null) {
+                    Toast.makeText(this , "Cancelled" , Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this , "Le code est : ${result.contents}" , Toast.LENGTH_LONG).show()
+                }
+            } else {
+                super.onActivityResult(requestCode , resultCode , data)
+            }
+        }
     }
 }
