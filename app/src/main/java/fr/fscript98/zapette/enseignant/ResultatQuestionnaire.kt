e81b265511ebe379e41dd49417fc624f7d18d790
franciscoantonio <francisco.skate.98@gmail.com>
2021-04-28 14:53:47 +0200

    Affichage avec histogramme

--- app/src/main/java/fr/fscript98/zapette/enseignant/ResultatQuestionnaire.kt
+++ app/src/main/java/fr/fscript98/zapette/enseignant/ResultatQuestionnaire.kt
@@ -9,6 +9,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
+import com.github.mikephil.charting.model.GradientColor
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.qrcode.QRCodeWriter
 import com.journeyapps.barcodescanner.BarcodeEncoder
@@ -32,25 +43,16 @@ class ResultatQuestionnaire : AppCompatActivity() {
         val repo1 = BddRepository()
         repo1.updateData {
             setContentView(R.layout.activity_resultat_questionnaire)
-            val textViewA = findViewById<TextView>(R.id.nbreponseA)
-            val textViewB = findViewById<TextView>(R.id.nbreponseB)
-            val textViewC = findViewById<TextView>(R.id.nbreponseC)
-            val textViewD = findViewById<TextView>(R.id.nbreponseD)
-            val textViewE = findViewById<TextView>(R.id.nbreponseE)
-            val textViewF = findViewById<TextView>(R.id.nbreponseF)
-            val textViewG = findViewById<TextView>(R.id.nbreponseG)
-            val textViewH = findViewById<TextView>(R.id.nbreponseH)
-            val textViewI = findViewById<TextView>(R.id.nbreponseI)
             val textViewTotal = findViewById<TextView>(R.id.nbTotal)
             val textView = findViewById<TextView>(R.id.textView2)
 
+            var barChart = findViewById<BarChart>(R.id.barChart)
+            var table = ArrayList<BarEntry>()
 
 
             for (codeBDD in questionListBdd) {
 
                 if (myRandomInt == codeBDD.motdepasse) {
-
-
                     val nbA = codeBDD.A
                     val nbB = codeBDD.B
                     val nbC = codeBDD.C
@@ -61,42 +63,91 @@ class ResultatQuestionnaire : AppCompatActivity() {
                     val nbH = codeBDD.H
                     val nbI = codeBDD.I
                     val nbTotal = nbA + nbB + nbC + nbD + nbE + nbF + nbG + nbH + nbI
-                    if (nbA != 0) {
-                        textViewA.text = ("$nbA")
-                    }
-                    if (nbB != 0) {
-                        textViewB.text = ("$nbB")
-                    }
-                    if (nbC != 0) {
-                        textViewC.text = ("$nbC")
-                    }
-                    if (nbD != 0) {
-                        textViewD.text = ("$nbD")
-                    }
-                    if (nbE != 0) {
-                        textViewE.text = ("$nbE")
-                    }
-                    if (nbF != 0) {
-                        textViewF.text = ("$nbF")
-                    }
 
 
-                    if (nbE != 0) {
-                        textViewE.text = ("$nbE")
-                    }
+                    table.add(BarEntry(1f,nbA.toFloat()))
+                    table.add(BarEntry(3f,nbB.toFloat()))
+                    table.add(BarEntry(5f,nbC.toFloat()))
+                    table.add(BarEntry(7f,nbD.toFloat()))
+                    table.add(BarEntry(9f,nbE.toFloat()))
+                    table.add(BarEntry(11f,nbF.toFloat()))
+                    table.add(BarEntry(13f,nbG.toFloat()))
+                    table.add(BarEntry(15f,nbH.toFloat()))
+                    table.add(BarEntry(17f,nbI.toFloat()))
 
-                    if (nbF != 0) {
-                        textViewF.text = ("$nbF")
-                    }
-                    if (nbG != 0) {
-                        textViewG.text = ("$nbG")
-                    }
-                    if (nbH != 0) {
-                        textViewH.text = ("$nbH")
-                    }
-                    if (nbI != 0) {
-                        textViewI.text = ("$nbI")
-                    }
+                    var barDataSet = BarDataSet(table,"")
+
+                    var barData = BarData(barDataSet)
+
+                    barChart.setFitBars(true)
+                    barChart.data = barData
+                    //barChart.animateY(1000)
+                    barChart.setDrawBarShadow(false)
+                    barChart.setDrawValueAboveBar(true)
+                    barChart.description.isEnabled=false
+
+                    barChart.setPinchZoom(false)
+                    barChart.isDoubleTapToZoomEnabled = false
+                    barChart.setScaleEnabled(false)
+
+                    barChart.setDrawGridBackground(false)
+                    barChart.isClickable=false
+
+                    //X Axis
+                    var labels = mutableListOf<String>()
+
+                    val xAxisFormatter: ValueFormatter = IndexAxisValueFormatter(labels)
+                    val xAxis = barChart.xAxis
+                    xAxis.setLabelCount(table.size,true)
+                    xAxis.valueFormatter = xAxisFormatter
+                    xAxis.labelCount = table.size
+
+                    xAxis.position = XAxis.XAxisPosition.BOTTOM
+                    xAxis.setDrawGridLines(false)
+                    xAxis.granularity = 2f
+                    xAxis.textColor = ContextCompat.getColor(this,R.color.white)
+                    xAxis.axisLineColor = ContextCompat.getColor(this,R.color.white)
+
+
+                    //Y Axis
+                    val leftAxis = barChart.axisLeft
+                    leftAxis.setLabelCount(0, true)
+                    leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART)
+                    leftAxis.setDrawLabels(false)
+                    leftAxis.axisLineColor = ContextCompat.getColor(this,R.color.white)
+                    leftAxis.spaceTop = 0f
+                    leftAxis.axisMinimum = 0f
+                    leftAxis.textColor = ContextCompat.getColor(this, R.color.white)
+                    //leftAxis.setDrawAxisLine(false)
+                    leftAxis.zeroLineColor = ContextCompat.getColor(this, R.color.white)
+
+                    val rightAxis = barChart.axisRight
+                    rightAxis.axisLineColor = ContextCompat.getColor(this,R.color.white)
+                    rightAxis.setDrawGridLines(false)
+                    rightAxis.setDrawLabels(false)
+                    rightAxis.setLabelCount(0, false)
+                    rightAxis.spaceTop = 15f
+                    rightAxis.axisMinimum = 0f
+
+
+                    val startColor1 = ContextCompat.getColor(this,R.color.lightblue3)
+                    val enColor1 = ContextCompat.getColor(this,R.color.lightblue3)
+
+                    val gradientColors: MutableList<GradientColor> = ArrayList()
+                    gradientColors.add(GradientColor(startColor1,enColor1))
+
+                    barDataSet.gradientColors = gradientColors
+
+                    val dataSets = ArrayList<IBarDataSet>()
+                    dataSets.add(barDataSet)
+
+                    val data = BarData(dataSets)
+                    data.setValueTextColor(ContextCompat.getColor(this,R.color.white))
+                    data.barWidth = 1.5f
+                    barChart.data = data
+
+                    barChart.legend.isEnabled=false
+
                     textViewTotal.text = ("$nbTotal")
                     questionModel = codeBDD
 
