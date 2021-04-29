70fb711794f964dc80534410d7f02f9059c8f1a5
Jesus420NoScope <julien.terrie@gmail.com>
2021-04-29 10:40:51 +0200

    c'est bon

--- app/src/main/java/fr/fscript98/zapette/eleve/EtudiantRepondre.kt
+++ app/src/main/java/fr/fscript98/zapette/eleve/EtudiantRepondre.kt
@@ -1,42 +1,39 @@
 package fr.fscript98.zapette.eleve
 
-
-import fr.fscript98.zapette.autre.BddRepository.Singleton.motDePasseBdd
-import fr.fscript98.zapette.autre.BddRepository.Singleton.question
-import fr.fscript98.zapette.autre.BddRepository.Singleton.questionListBdd
-import fr.fscript98.zapette.autre.BddRepository.Singleton.ref_questionnaire
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.Toast
-import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.qrcode.QRCodeWriter
 import com.journeyapps.barcodescanner.BarcodeEncoder
-import fr.fscript98.zapette.MainActivity
-import fr.fscript98.zapette.autre.QuestionModel
 import fr.fscript98.zapette.R
 import fr.fscript98.zapette.autre.BddRepository
+import fr.fscript98.zapette.autre.BddRepository.Singleton.motDePasseBdd
+import fr.fscript98.zapette.autre.BddRepository.Singleton.questionListBdd
+import fr.fscript98.zapette.autre.QuestionModel
 import fr.fscript98.zapette.eleve.EtudiantRepondre.Singleton.bitMap
 import fr.fscript98.zapette.eleve.EtudiantRepondre.Singleton.derniereRep
-import fr.fscript98.zapette.enseignant.ResultatQuestionnaire.Singleton.questionModel
+import fr.fscript98.zapette.eleve.EtudiantRepondre.Singleton.questionM
 
 
 class EtudiantRepondre : AppCompatActivity() {
 
     object Singleton {
-        lateinit var bitMap : Bitmap
+        lateinit var questionM: QuestionModel
+        lateinit var bitMap: Bitmap
         var derniereRep = ""
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+        val repo1 = BddRepository()
+
         setContentView(R.layout.activity_etudiant_repondre)
         // view
         val a = findViewById<Button>(R.id.buttonA)
@@ -64,22 +61,30 @@ class EtudiantRepondre : AppCompatActivity() {
         val refQuestionnaire = database.getReference("questionnaire")
         var oldButtonClique = "null"
 
+        repo1.updateData {
+            for (question in questionListBdd) {
+                if (question.motdepasse.toString() == motDePasseBdd) {
+                    questionM = question
+                    if (question.questionTerminee == "true") {
+                        val intent = Intent(this , EtudiantResultats::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+            }
+        }
+
         fun fonction(buttonClique: String) {
             database.getReference("questionnaire").get().addOnSuccessListener {
-                // On récup les enfant du chemin questionnaire
                 for (child in it.children) {
                     val questionModel = child.getValue(QuestionModel::class.java)
                     if (questionModel != null) {
-                        // On compare avec le mot de passe de la page EtudiantQuestinnaire
                         if (motDePasseBdd == questionModel.motdepasse.toString()) {
-                            //Toast.makeText(applicationContext,child.child(buttonClique).value,LENGTH_SHORT).show()
-                            // id correspond a la valeur du motDePasse pour la question a laquelle tu as participé
                             if (derniereRep != "") {
                                 val numb1 = child.child(derniereRep).value.toString().toInt()
                                 refQuestionnaire.child(child.ref.key.toString())
                                     .child(derniereRep).setValue(numb1 - 1)
                             }
-
                             val numb = child.child(buttonClique).value.toString().toInt()
                             refQuestionnaire.child(child.ref.key.toString())
                                 .child(buttonClique).setValue(numb + 1)
@@ -89,124 +94,119 @@ class EtudiantRepondre : AppCompatActivity() {
                         }
                     }
                 }
-
             }
         }
 
-            buttonBack.setOnClickListener {
-                val intentButtonBack = Intent(this , EtudiantQuestionnaire::class.java)
-                startActivity(intentButtonBack)
-                finish()
-            }
-
-            // changer la couleur de l'écriture pour voir la sélection
-            a.setOnClickListener {
-                val baise = questionModel.questionTerminee
-                if (baise == "false")
-                    Toast.makeText(this , "pd" , LENGTH_SHORT).show()
-
-                // si le boutton cliqué et déjà A alors on remet le texte a noir et la valeur buttonClique a "aucun"
-                if (oldButtonClique != "A") fonction("A")
-                //si on clique sur A , on change la couler de A et on remet les autres en Noir
-                for (button in buttonList) {
-                    button.setBackgroundColor(Color.WHITE)
-                }
-                a.setBackgroundColor(Color.parseColor("#FFBB86FC"))
-                //buttonClique = "A"
-
-
-            }
-
-            b.setOnClickListener {
-                if (oldButtonClique != "B") fonction("B")
-
-                for (button in buttonList) {
-                    button.setBackgroundColor(Color.WHITE)
-                }
-                b.setBackgroundColor(Color.parseColor("#FFBB86FC"))
-                //buttonClique = "B"
-
-            }
-
-            c.setOnClickListener {
-                if (oldButtonClique != "C") fonction("C")
-
-                for (button in buttonList) {
-                    button.setBackgroundColor(Color.WHITE)
-                }
-                c.setBackgroundColor(Color.parseColor("#FFBB86FC"))
-                //buttonClique = "C"
-            }
-
-            d.setOnClickListener {
-                if (oldButtonClique != "D") fonction("D")
-
-                for (button in buttonList) {
-                    button.setBackgroundColor(Color.WHITE)
-                }
-                d.setBackgroundColor(Color.parseColor("#FFBB86FC"))
-            }
-
-            e.setOnClickListener {
-                if (oldButtonClique != "E") fonction("E")
-
-                for (button in buttonList) {
-                    button.setBackgroundColor(Color.WHITE)
-                }
-                e.setBackgroundColor(Color.parseColor("#FFBB86FC"))
-            }
-
-            f.setOnClickListener {
-                if (oldButtonClique != "F") fonction("F")
-                for (button in buttonList) {
-                    button.setBackgroundColor(Color.WHITE)
-                }
-                f.setBackgroundColor(Color.parseColor("#FFBB86FC"))
-            }
-
-            g.setOnClickListener {
-                if (oldButtonClique != "G") fonction("G")
-                for (button in buttonList) {
-                    button.setBackgroundColor(Color.WHITE)
-                }
-                g.setBackgroundColor(Color.parseColor("#FFBB86FC"))
-            }
-
-            h.setOnClickListener {
-                if (oldButtonClique != "H") fonction("H")
-                for (button in buttonList) {
-                    button.setBackgroundColor(Color.WHITE)
-                }
-                h.setBackgroundColor(Color.parseColor("#FFBB86FC"))
-            }
-
-            i.setOnClickListener {
-                if (oldButtonClique != "I") fonction("I")
-                for (button in buttonList) {
-                    button.setBackgroundColor(Color.WHITE)
-                }
-                i.setBackgroundColor(Color.parseColor("#FFBB86FC"))
-            }
-
-            val qrCode = QRCodeWriter()
-            val qrCodePage = Intent(this , QrCode::class.java)
-            val bitMtx = qrCode.encode(
-                motDePasseBdd ,
-                BarcodeFormat.QR_CODE ,
-                2000 ,
-                2000
-            )
-
-            val imageCode = findViewById<ImageView>(R.id.imageQrCodeEleve)
-            val barcodeEncoder = BarcodeEncoder()
-            bitMap = barcodeEncoder.createBitmap(bitMtx)
-            imageCode.setImageBitmap(bitMap)
-            imageCode.setOnClickListener {
-                startActivity(qrCodePage)
-            }
+        buttonBack.setOnClickListener {
+            val intentButtonBack = Intent(this , EtudiantQuestionnaire::class.java)
+            startActivity(intentButtonBack)
+            finish()
         }
 
+        // changer la couleur de l'écriture pour voir la sélection
+        a.setOnClickListener {
+            // si le boutton cliqué et déjà A alors on remet le texte a noir et la valeur buttonClique a "aucun"
+            if (oldButtonClique != "A") fonction("A")
+            //si on clique sur A , on change la couler de A et on remet les autres en Noir
+            for (button in buttonList) {
+                button.setBackgroundColor(Color.WHITE)
+            }
+            a.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+            //buttonClique = "A"
+
+
+        }
+
+        b.setOnClickListener {
+            if (oldButtonClique != "B") fonction("B")
+
+            for (button in buttonList) {
+                button.setBackgroundColor(Color.WHITE)
+            }
+            b.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+            //buttonClique = "B"
+
+        }
+
+        c.setOnClickListener {
+            if (oldButtonClique != "C") fonction("C")
+
+            for (button in buttonList) {
+                button.setBackgroundColor(Color.WHITE)
+            }
+            c.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+            //buttonClique = "C"
+        }
+
+        d.setOnClickListener {
+            if (oldButtonClique != "D") fonction("D")
+
+            for (button in buttonList) {
+                button.setBackgroundColor(Color.WHITE)
+            }
+            d.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+        }
+
+        e.setOnClickListener {
+            if (oldButtonClique != "E") fonction("E")
+
+            for (button in buttonList) {
+                button.setBackgroundColor(Color.WHITE)
+            }
+            e.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+        }
+
+        f.setOnClickListener {
+            if (oldButtonClique != "F") fonction("F")
+            for (button in buttonList) {
+                button.setBackgroundColor(Color.WHITE)
+            }
+            f.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+        }
+
+        g.setOnClickListener {
+            if (oldButtonClique != "G") fonction("G")
+            for (button in buttonList) {
+                button.setBackgroundColor(Color.WHITE)
+            }
+            g.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+        }
+
+        h.setOnClickListener {
+            if (oldButtonClique != "H") fonction("H")
+            for (button in buttonList) {
+                button.setBackgroundColor(Color.WHITE)
+            }
+            h.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+        }
+
+        i.setOnClickListener {
+            if (oldButtonClique != "I") fonction("I")
+            for (button in buttonList) {
+                button.setBackgroundColor(Color.WHITE)
+            }
+            i.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+        }
+
+        val qrCode = QRCodeWriter()
+        val qrCodePage = Intent(this , QrCode::class.java)
+        val bitMtx = qrCode.encode(
+            motDePasseBdd ,
+            BarcodeFormat.QR_CODE ,
+            2000 ,
+            2000
+        )
+
+        val imageCode = findViewById<ImageView>(R.id.imageQrCodeEleve)
+        val barcodeEncoder = BarcodeEncoder()
+        bitMap = barcodeEncoder.createBitmap(bitMtx)
+        imageCode.setImageBitmap(bitMap)
+        imageCode.setOnClickListener {
+            startActivity(qrCodePage)
+        }
+    }
 }
+
 
 
 
