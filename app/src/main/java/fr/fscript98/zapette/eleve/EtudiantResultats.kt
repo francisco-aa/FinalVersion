a498ceea1230edd87f04628843c803c324afafd8
Jesus420NoScope <julien.terrie@gmail.com>
2021-05-01 12:52:19 +0200

    etudiantResultats

--- app/src/main/java/fr/fscript98/zapette/eleve/EtudiantResultats.kt
+++ app/src/main/java/fr/fscript98/zapette/eleve/EtudiantResultats.kt
@@ -1,6 +1,7 @@
 package fr.fscript98.zapette.eleve
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
@@ -8,44 +9,57 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import fr.fscript98.zapette.MainActivity
 import fr.fscript98.zapette.R
+import fr.fscript98.zapette.eleve.EtudiantRepondre.Singleton.derniereRep
+import fr.fscript98.zapette.eleve.EtudiantRepondre.Singleton.questionM
 import java.lang.Thread.sleep
 import android.view.animation.AlphaAnimation as AlphaAnimation1
 
 class EtudiantResultats : AppCompatActivity() {
 
-    fun load(){
-        val v = findViewById<TextView>(R.id.rep1)
-        val anim = AlphaAnimation1(0.0f , 1.0f)
-        anim.duration = 2000
-        anim.repeatCount = 0
-        v.startAnimation(anim)
-
-        val v3 = findViewById<CardView>(R.id.rep1_card)
-        val anim3 = AlphaAnimation1(0.0f , 1.0f)
-        anim3.duration = 2000
-        anim3.repeatCount = 0
-        v3.startAnimation(anim3)
-
-        sleep(1000)
-
-        val v2 = findViewById<TextView>(R.id.rep2)
-        val anim2 = AlphaAnimation1(0.0f , 1.0f)
-        anim2.duration = 3000
-        anim2.repeatCount = 0
-        v2.startAnimation(anim2)
-
-        val v4 = findViewById<CardView>(R.id.rep2_card)
-        val anim4 = AlphaAnimation1(0.0f , 1.0f)
-        anim4.duration = 3000
-        anim4.repeatCount = 0
-        v4.startAnimation(anim4)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_etudiant_resultats)
 
-       load()
+        val rep1 = findViewById<TextView>(R.id.rep1)
+        val rep1_card = findViewById<CardView>(R.id.rep1_card)
+        val rep2 = findViewById<TextView>(R.id.rep2)
+        val rep2_card = findViewById<CardView>(R.id.rep2_card)
+        //val reponse = questionM.bonneReponse
+        val reponse = "A"
+
+
+        //rep1.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+        rep2_card.setCardBackgroundColor(Color.GREEN)
+        rep1.setText(derniereRep)
+        rep2.setText(reponse)
+
+        if (derniereRep == reponse){
+            rep1_card.setCardBackgroundColor(Color.GREEN)
+        }
+        else{
+            rep1_card.setCardBackgroundColor(Color.RED)
+        }
+
+
+        val anim = AlphaAnimation1(0.0f , 1.0f)
+        anim.duration = 2000
+        anim.repeatCount = 0
+        rep1.startAnimation(anim)
+
+        val anim3 = AlphaAnimation1(0.0f , 1.0f)
+        anim3.duration = 2000
+        anim3.repeatCount = 0
+        rep1_card.startAnimation(anim3)
+
+        val anim2 = AlphaAnimation1(0.0f , 1.0f)
+        anim2.duration = 3000
+        anim2.repeatCount = 0
+        rep2.startAnimation(anim2)
+
+        val anim4 = AlphaAnimation1(0.0f , 1.0f)
+        anim4.duration = 3000
+        anim4.repeatCount = 0
+        rep2_card.startAnimation(anim4)
 
         val backbutton = findViewById<ImageView>(R.id.button_back)
         backbutton.setOnClickListener {
@@ -53,6 +67,7 @@ class EtudiantResultats : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
     }
 
     override fun onBackPressed() {
