ef6949dd57f6a9c38cfdea4ddd7c8403eff6f1b2
Jesus420NoScope <julien.terrie@gmail.com>
2021-05-04 15:37:50 +0200

    resultats

--- app/src/main/java/fr/fscript98/zapette/eleve/EtudiantResultats.kt
+++ app/src/main/java/fr/fscript98/zapette/eleve/EtudiantResultats.kt
@@ -26,7 +26,6 @@ import android.view.animation.AlphaAnimation as AlphaAnimation1
 class EtudiantResultats : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_etudiant_resultats)
@@ -52,90 +51,95 @@ class EtudiantResultats : AppCompatActivity() {
             finish()
         }
 
+        fun oh(rep: Boolean) {
+            GlobalScope.launch(context = Dispatchers.Main) {
+                rep1.setText(reponseEtudiant)
+                rep2.setText(bonneReponse)
+                val anim = AlphaAnimation1(0.0f , 1.0f)
+                anim.duration = 1000
+                anim.repeatCount = 0
+                anim.startOffset = 0
+                rep1.startAnimation(anim)
+
+                val anim2 = AlphaAnimation1(0.0f , 1.0f)
+                anim2.duration = 1000
+                anim2.repeatCount = 0
+                anim2.startOffset = 0
+                rep1_card.startAnimation(anim2)
+
+                val anim3 = AlphaAnimation1(0.0f , 1.0f)
+                anim3.duration = 1000
+                anim3.repeatCount = 0
+                anim3.startOffset = 1500
+                rep2.startAnimation(anim3)
+
+                val anim4 = AlphaAnimation1(0.0f , 1.0f)
+                anim4.duration = 1000
+                anim4.repeatCount = 0
+                anim4.startOffset = 1500
+                rep2_card.startAnimation(anim4)
+            }
+
+            if (!rep)
+                GlobalScope.launch(context = Dispatchers.Main) {
+                    delay(2500)
+                    rep1_card.setCardBackgroundColor(Color.RED)
+                }
+            else
+                GlobalScope.launch(context = Dispatchers.Main) {
+                    val viewKonfetti = findViewById<KonfettiView>(R.id.viewKonfetti)
+                    delay(2500)
+                    rep1_card.setCardBackgroundColor(Color.GREEN)
+
+                    viewKonfetti.build()
+                        .addColors(
+                            Color.YELLOW ,
+                            Color.GREEN ,
+                            Color.MAGENTA ,
+                            Color.CYAN ,
+                            Color.RED ,
+                            Color.BLUE
+                        )
+                        .setDirection(250.0 , 280.0)
+                        .setSpeed(16f , 12f)
+                        .setFadeOutEnabled(true)
+                        .setTimeToLive(2000L)
+                        .addShapes(Shape.Square , Shape.Circle)
+                        .addSizes(Size(8))
+                        .setPosition(1140f , null , 2200f , null)
+                        .streamFor(200 , 1000L)
+
+                    viewKonfetti.build()
+                        .addColors(
+                            Color.YELLOW ,
+                            Color.GREEN ,
+                            Color.MAGENTA ,
+                            Color.CYAN ,
+                            Color.RED ,
+                            Color.BLUE
+                        )
+                        .setDirection(270.0 , 290.0)
+                        .setSpeed(16f , 12f)
+                        .setFadeOutEnabled(true)
+                        .setTimeToLive(2000L)
+                        .addShapes(Shape.Square , Shape.Circle)
+                        .addSizes(Size(8))
+                        .setPosition(-50f , null , 2200f , null)
+                        .streamFor(200 , 1000L)
+                }
+        }
+
         refQuestionnaire.get().addOnSuccessListener {
             for (question in it.children) {
                 if (motDePasseBdd == question.child("motdepasse").value.toString()) {
                     bonneReponse = question.child("bonneReponse").value.toString()
-                    reponseEtudiant = sharedPreferences.getString(question.key.toString() , "").toString()
-                    if (bonneReponse == reponseEtudiant) {
-                        //coroutine 1
-                        GlobalScope.launch(context = Dispatchers.Main) {
-                            rep1.setText(reponseEtudiant)
-                            rep2.setText(bonneReponse)
-                            val anim = AlphaAnimation1(0.0f , 1.0f)
-                            anim.duration = 1000
-                            anim.repeatCount = 0
-                            anim.startOffset = 0
-                            rep1.startAnimation(anim)
+                    reponseEtudiant =
+                        sharedPreferences.getString(question.key.toString() , "").toString()
 
-                            val anim2 = AlphaAnimation1(0.0f , 1.0f)
-                            anim2.duration = 1000
-                            anim2.repeatCount = 0
-                            anim2.startOffset = 0
-                            rep1_card.startAnimation(anim2)
-
-                            val anim3 = AlphaAnimation1(0.0f , 1.0f)
-                            anim3.duration = 1000
-                            anim3.repeatCount = 0
-                            anim3.startOffset = 1500
-                            rep2.startAnimation(anim3)
-
-                            val anim4 = AlphaAnimation1(0.0f , 1.0f)
-                            anim4.duration = 1000
-                            anim4.repeatCount = 0
-                            anim4.startOffset = 1500
-                            rep2_card.startAnimation(anim4)
-                        }
-
-                        GlobalScope.launch(context = Dispatchers.Main) {
-                            val viewKonfetti = findViewById<KonfettiView>(R.id.viewKonfetti)
-                            delay(2500)
-                            rep1_card.setCardBackgroundColor(Color.GREEN)
-
-                            viewKonfetti.build()
-                                .addColors(
-                                    Color.YELLOW ,
-                                    Color.GREEN ,
-                                    Color.MAGENTA ,
-                                    Color.CYAN ,
-                                    Color.RED ,
-                                    Color.BLUE
-                                )
-                                .setDirection(250.0, 280.0)
-                                .setSpeed(16f , 12f)
-                                .setFadeOutEnabled(true)
-                                .setTimeToLive(2000L)
-                                .addShapes(Shape.Square , Shape.Circle)
-                                .addSizes(Size(8))
-                                .setPosition(1140f, null , 2200f , null)
-                                .streamFor(200 , 1000L)
-
-                            viewKonfetti.build()
-                                .addColors(
-                                    Color.YELLOW ,
-                                    Color.GREEN ,
-                                    Color.MAGENTA ,
-                                    Color.CYAN ,
-                                    Color.RED ,
-                                    Color.BLUE
-                                )
-                                .setDirection(270.0, 290.0)
-                                .setSpeed(16f , 12f)
-                                .setFadeOutEnabled(true)
-                                .setTimeToLive(2000L)
-                                .addShapes(Shape.Square , Shape.Circle)
-                                .addSizes(Size(8))
-                                .setPosition(-50f, null , 2200f , null)
-                                .streamFor(200 , 1000L)
-                        }
-
-
-
-                    }
-                    else{
-                        rep1_card.setCardBackgroundColor(Color.RED)
-                        rep2_card.setCardBackgroundColor(Color.GREEN)
-                    }
+                    if (bonneReponse == reponseEtudiant && bonneReponse != "")
+                        oh(true)
+                    else if (bonneReponse != "")
+                        oh(false)
                 }
             }
         }
